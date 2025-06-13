8fce374d510dd0bd2042472328342e159c0cf539
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-06-13 22:40:51 +0500

    feat: Hero Section Animations using Motion, Snap-scrolling and react-typing-animations with Responsive Layout.

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "motion/react";
+
+const Speech = () => {
+  return (
+    <motion.div
+      className="bubbleContainer"
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+    >
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Came for hover effects, Stayed for wiggly 3D shapes.",
+            1000,
+            "I like bending spaces in the browser.",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+      </div>
+      <img src="/juni.PNG" alt="" />
+    </motion.div>
+  );
+};
+
+export default Speech;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Portfolio.css";
+const Portfolio = () => {
+  return <div className="portfolio">Port</div>;
+};
+
+export default Portfolio;
